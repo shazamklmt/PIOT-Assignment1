acd529fac7de01e7a1b96f0405ef411caa8afce5
s3660457 <unconfigured@null.spigotmc.org>
2020-03-24 23:21:06 +1100

    Update

--- Task B/monitorAndDisplay.py
+++ Task B/monitorAndDisplay.py
@@ -1,6 +1,41 @@
 from sense_hat import SenseHat
 import json, os
 
+b = [0,0,255]
+led_blue [
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+    b,b,b,b,b,b,b,b
+]
+g = [0,255,0]
+led_green [
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+    g,g,g,g,g,g,g,g
+]
+r = [255,0,0]
+led_red [
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+    r,r,r,r,r,r,r,r
+]
+
+
 # Get the file contents
 def get_file_contents():
     readFile = open("Task B/config.json", "r")
